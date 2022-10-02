eff62d4fa744609e2dc5bd117b0a9ef9cccf77ed
apiscevs <aleksejs.piscevs@gmail.com>
2022-10-02 12:52:20 +0200

    added commands service, added NodePort for platform service kubernetes

---
+++ microservices-kubernetes/CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[ApiController]
+[Route("api/c/[controller]")]
+public class CommandsController : ControllerBase
+{
+    public CommandsController()
+    {
+
+    }
+}
+
---
+++ microservices-kubernetes/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[ApiController]
+[Route("api/c/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult> TestInboundConnection()
+    {
+        Console.WriteLine("Hello from TestInboundConnection");
+        return Ok("Hello from TestInboundConnection");
+    }
+
+}
+
